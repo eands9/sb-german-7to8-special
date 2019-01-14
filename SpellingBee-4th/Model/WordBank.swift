c678e1476f9327aa3b431147749b51d5bd6fc6f6
Eric Hernandez <eands9@yahoo.com>
2019-01-14 10:48:54 -0600

    special8special

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,15 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"seltzer", sentence:""))
-        list.append(Word(word:"schadenfreude", sentence:""))
-        list.append(Word(word:"dreidel", sentence:""))
-        list.append(Word(word:"weimaraner", sentence:""))
-        list.append(Word(word:"ersatz", sentence:""))
-        list.append(Word(word:"fräulein", sentence:""))
-        list.append(Word(word:"blitzkrieg", sentence:""))
-        list.append(Word(word:"gesundheit", sentence:""))
-        list.append(Word(word:"pfeffernuss", sentence:""))
-        list.append(Word(word:"glockenspiel", sentence:""))
+        list.append(Word(word:"schottische", sentence:""))
+        list.append(Word(word:"anschluss", sentence:""))
+        list.append(Word(word:"springerle", sentence:""))
+        list.append(Word(word:"zeitgeber", sentence:""))
+        list.append(Word(word:"realschule", sentence:""))
+        list.append(Word(word:"stollen", sentence:""))
+        list.append(Word(word:"lederhosen", sentence:""))
+        list.append(Word(word:"autobahn", sentence:""))
+        list.append(Word(word:"echt", sentence:""))
+        list.append(Word(word:"rottweiler", sentence:""))
     }
 }
